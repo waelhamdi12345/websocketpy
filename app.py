ef5119cd77f2b3939441c6f4c3d4162e25bf4821
waelhamdi12345 <121729150+waelhamdi12345@users.noreply.github.com>
2024-03-23 12:02:36 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import asyncio
+import websockets
+
+connected_clients = {}
+
+class MyWebSocket:
+    async def __call__(self, websocket, path):
+        connected_clients[id(websocket)] = websocket
+        print(f"New connection! ({id(websocket)})")
+        await websocket.send("Welcome to the Server.")
+        try:
+            async for message in websocket:
+                await self.send_to_all(message)
+        finally:
+            del connected_clients[id(websocket)]
+
+    async def send_to_all(self, message):
+        for client in connected_clients.values():
+            await client.send(message)
+
+start_server = websockets.serve(MyWebSocket(), "192.168.50.102", 5000)
+
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
